b42e9906d448ecb8e5bfd5f6537addb425f80c6d
sofienazzabi2 <140543542+sofienazzabi2@users.noreply.github.com>
2024-12-08 21:35:34 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,7 +10,8 @@ output "aks_node_rg" {
   value = azurerm_kubernetes_cluster.aks.node_resource_group # No update needed
 }
 
-resource "local_file" "kubeconfig" {
+# Renommé la ressource pour éviter les duplications
+resource "local_file" "kubeconfig_output" {
   filename = "kubeconfig" # Optional: Update the output file name if needed
   content  = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
