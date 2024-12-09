ebc85b9ec4a04c5ad8212b4fbd9318d46e2acd5b
sofienazzabi2 <140543542+sofienazzabi2@users.noreply.github.com>
2024-12-09 13:25:56 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,17 +1,17 @@
 output "aks_id" {
-  value = azurerm_kubernetes_cluster.aks.id # No update needed
+  value = azurerm_kubernetes_cluster.aks[0].id
 }
 
 output "aks_fqdn" {
-  value = azurerm_kubernetes_cluster.aks.fqdn # No update needed
+  value = azurerm_kubernetes_cluster.aks[0].fqdn
 }
 
 output "aks_node_rg" {
-  value = azurerm_kubernetes_cluster.aks.node_resource_group # No update needed
+  value = azurerm_kubernetes_cluster.aks[0].node_resource_group
 }
 
-# Renommé la ressource pour éviter les duplications
 resource "local_file" "kubeconfig_output" {
-  filename = "kubeconfig" # Optional: Update the output file name if needed
-  content  = azurerm_kubernetes_cluster.aks.kube_config_raw
+  filename = "kubeconfig"
+  content  = azurerm_kubernetes_cluster.aks[0].kube_config_raw
 }
+
